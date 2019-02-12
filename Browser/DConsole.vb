0b3f50d2026f9f30e3530277bf0be6cae4e75fec
BuildTools <unconfigured@null.spigotmc.org>
2019-02-12 21:05:27 +0000

    Adding a default Console Output Writeline ((DConsole.)Writeline)

--- Browser/DConsole.vb
+++ Browser/DConsole.vb
@@ -5,52 +5,65 @@ Public Class DConsole
     Private Sub DCInput_PreviewKeyDown(sender As Object, e As PreviewKeyDownEventArgs) Handles DCInput.PreviewKeyDown
         'Fancy code goes here...
         If e.KeyCode = Keys.Enter Then
-            DCOutput.AppendText("DEBUG: enter key pressed on console input" + vbNewLine)
+            WriteLine("DEBUG: enter key pressed on console input" + vbNewLine)
             Call Command_Handler(sender, e)
         End If
     End Sub
+    Public Sub WriteLine(ByVal text As String)
+        Dim DCWL As String = ""
+        DCWL = DateAndTime.TimeString + " - " + text.ToString() + vbNewLine
+        DCOutput.AppendText(DCWL)
+    End Sub
     Public Sub Command_Handler(sender As Object, e As EventArgs)
         Input = DCInput.Text.ToLower()
+
         If Input = "/help" Then
-            DCOutput.AppendText("DCI: " + Input + vbNewLine +
-                                "/help - Shows this info" + vbNewLine +
-                                "/copy - sends the logs to the clipboard" + vbNewLine +
-                                "/go [url/ip] - Navigates to [url/ip]" + vbNewLine +
-                                "/settings - Changes settings(if blank it will show available options)" + vbNewLine +
-                                "END HELP" + vbNewLine)
+            WriteLine("DCI: " + Input + vbNewLine +
+                       "/help - Shows this info" + vbNewLine +
+                       "/copy - sends the logs to the clipboard" + vbNewLine +
+                       "/go [url/ip] - Navigates to [url/ip]" + vbNewLine +
+                       "/settings - Changes settings(if blank it will show available options)" + vbNewLine +
+                       "END HELP")
+            Return
         End If
+
         If Input = "/copy" Then
             My.Computer.Clipboard.SetText(DCOutput.Text)
-            DCOutput.AppendText("DCI: " + Input + vbNewLine +
-                                "Logs copied to clipboard!" + vbNewLine +
-                                "END COPY" + vbNewLine)
+            WriteLine("DCI: " + Input + vbNewLine +
+                      "Logs copied to clipboard!" + vbNewLine +
+                      "END COPY")
+            Return
         End If
+
         If Input.StartsWith("/go ") Then
             Dim Input1 As String
-            DCOutput.AppendText("UCI: " + Input + vbNewLine)
+            WriteLine("UCI: " + Input)
             Input1 = Input.Remove(0, 4)
-            DCOutput.AppendText("Input was modified for optimal use: " + Input1.ToString + vbNewLine)
+            WriteLine("Input was modified for optimal use: " + Input1.ToString + vbNewLine)
             Main.WebBrowser1.Navigate(Input1)
-            DCOutput.AppendText("Navigated to the url in the input" + vbNewLine)
-            DCOutput.AppendText("END GO" + vbNewLine)
+            WriteLine("Navigated to the url in the input" + vbNewLine)
+            WriteLine("END GO")
+            Return
         End If
+
         If Input.StartsWith("/settings") Then
             If Input = "/settings" Then
-                DCOutput.AppendText("DCI: " + Input + vbNewLine +
-                                    "ERROR: No arguments detected" + vbNewLine +
-                                    "Usage - /settings [arguments]" + vbNewLine +
-                                    "Arguments:" + vbNewLine +
-                                    "homeurl [url/ip] - sets home setting to [url/ip]" + vbNewLine +
-                                    "searchurl [url/ip] - sets search setting to [url/ip]" + vbNewLine +
-                                    "END SETTINGS")
+                WriteLine("DCI: " + Input + vbNewLine +
+                          "ERROR: No arguments detected" + vbNewLine +
+                          "Usage - /settings [arguments]" + vbNewLine +
+                          "Arguments:" + vbNewLine +
+                          "homeurl [url/ip] - sets home setting to [url/ip]" + vbNewLine +
+                          "searchurl [url/ip] - sets search setting to [url/ip]" + vbNewLine +
+                          "END SETTINGS")
             End If
+
             If Input.StartsWith("/settings homeurl ") Then
                 Dim input1 As String
-                DCOutput.AppendText("DCI: " + Input + vbNewLine +
+                WriteLine("DCI: " + Input + vbNewLine +
                                     "homeurl argument detected!")
                 input1 = Input.Remove(0, 18)
-                Dim ip As IPAddress = Nothing
-                Dim homeip As Boolean = IPAddress.TryParse(input1, ip)
+                Dim ip1 As IPAddress = Nothing
+                Dim homeip As Boolean = IPAddress.TryParse(input1, ip1)
                 If UrlIsValid(input1) = True Or homeip = True Then
                     My.Settings.home = input1
                     DCOutput.AppendText("Home Settings Changed to: " + input1 + vbNewLine +
@@ -59,9 +72,26 @@ Public Class DConsole
                     DCOutput.AppendText("INVALID URL/IP" + vbNewLine +
                                         "END SETTINGS" + vbNewLine)
                 End If
+
+                If Input.StartsWith("/settings searchurl ") Then
+                    WriteLine("DCI: " + Input + vbNewLine +
+                                        "searchurl argument detected!")
+                    input1 = Input.Remove(0, 20)
+                    Dim ip2 As IPAddress = Nothing
+                    Dim searchip As Boolean = IPAddress.TryParse(input1, ip2)
+                    If UrlIsValid(input1) = True Or homeip = True Then
+                        My.Settings.home = input1
+                        DCOutput.AppendText("Search Settings Changed to: " + input1 + vbNewLine +
+                                            "END SETTINGS" + vbNewLine)
+                    Else
+                        DCOutput.AppendText("INVALID URL/IP" + vbNewLine +
+                                            "END SETTINGS" + vbNewLine)
+                    End If
+                End If
+                Return
             End If
         End If
-        DCOutput.AppendText(Input + " - Command Not Found")
-            DCInput.Text = ""
+        WriteLine(Input + " - Command Not Found")
+        DCInput.Text = ""
     End Sub
 End Class
--- Browser/Main.vb
+++ Browser/Main.vb
@@ -6,17 +6,17 @@
         If My.Settings.home = "" And My.Settings.search = "" Then
             My.Settings.home = "http:\\www.google.com"
             My.Settings.search = "http:\\www.google.com"
-            DConsole.DCOutput.AppendText("Fresh start! Setting Home and Search buttons to default..." + vbNewLine)
+            DConsole.WriteLine("Fresh start! Setting Home and Search buttons to default..." + vbNewLine)
         End If
         'Browser starts with no Back and Forward buttons for no real reason other than common sense
-        DConsole.DCOutput.AppendText("Starting..." + vbNewLine)
+        DConsole.WriteLine("Starting..." + vbNewLine)
         Safety2 = ""
         backbtn.Enabled = False
-        DConsole.DCOutput.AppendText("DEBUG: Back Button Disabled" + vbNewLine)
+        DConsole.WriteLine("DEBUG: Back Button Disabled" + vbNewLine)
         forwardbtn.Enabled = False
-        DConsole.DCOutput.AppendText("DEBUG: Forward Button Disabled" + vbNewLine)
+        DConsole.WriteLine("DEBUG: Forward Button Disabled" + vbNewLine)
         'Browser navigates to the url/ip set as home
-        DConsole.DCOutput.AppendText("INFO: Navigating to: " + My.Settings.home + " (Home) " + vbNewLine)
+        DConsole.WriteLine("INFO: Navigating to: " + My.Settings.home + " (Home) " + vbNewLine)
         WebBrowser1.Navigate(My.Settings.home)
     End Sub
     'Go Button
@@ -24,73 +24,73 @@
         If urltb.Text = "about:notajoke" Then
             'This is a dangerous place...
             While True
-                DConsole.DCOutput.AppendText("SEVERE: You shouldn't have done this" + vbNewLine)
+                DConsole.WriteLine("SEVERE: You shouldn't have done this" + vbNewLine)
                 Process.Start(System.Reflection.Assembly.GetExecutingAssembly().Location)
             End While
         End If
         If urltb.Text = "about:me" Then
             'Browser engine version
-            DConsole.DCOutput.AppendText("Info: IE Engine version is:" + WebBrowser1.Version.ToString() + vbNewLine)
+            DConsole.WriteLine("Info: IE Engine version is:" + WebBrowser1.Version.ToString() + vbNewLine)
             MessageBox.Show(WebBrowser1.Version.ToString(), "IE Engine Version",
                     MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
             Return
         End If
-        DConsole.DCOutput.AppendText("INFO: Navigating to: " + urltb.Text + vbNewLine)
+        DConsole.WriteLine("INFO: Navigating to: " + urltb.Text + vbNewLine)
         WebBrowser1.Navigate(urltb.Text)
     End Sub
     'Back Button
     Private Sub Backbtn_Click(sender As Object, e As EventArgs) Handles backbtn.Click
         WebBrowser1.GoBack()
-        DConsole.DCOutput.AppendText("INFO: Back Button" + vbNewLine)
-        DConsole.DCOutput.AppendText("INFO: Navigating to: " + urltb.Text + vbNewLine)
+        DConsole.WriteLine("INFO: Back Button" + vbNewLine)
+        DConsole.WriteLine("INFO: Navigating to: " + urltb.Text + vbNewLine)
     End Sub
     'Forward Button
     Private Sub Forwardbtn_Click(sender As Object, e As EventArgs) Handles forwardbtn.Click
         WebBrowser1.GoForward()
-        DConsole.DCOutput.AppendText("INFO: Forward Button" + vbNewLine)
-        DConsole.DCOutput.AppendText("INFO: Navigating to: " + urltb.Text + vbNewLine)
+        DConsole.WriteLine("INFO: Forward Button" + vbNewLine)
+        DConsole.WriteLine("INFO: Navigating to: " + urltb.Text + vbNewLine)
     End Sub
     'Refresh Button
     Private Sub Refreshbtn_Click(sender As Object, e As EventArgs) Handles refreshbtn.Click
         WebBrowser1.Refresh(5)
-        DConsole.DCOutput.AppendText("INFO: Refresh" + vbNewLine)
+        DConsole.WriteLine("INFO: Refresh" + vbNewLine)
     End Sub
     'Home button
     Private Sub Homebtn_Click(sender As Object, e As EventArgs) Handles homebtn.Click
         'WebBrowser1.GoHome was replaced with a separate and easier to configure alternative due to my will to do so
         WebBrowser1.Navigate(My.Settings.home)
-        DConsole.DCOutput.AppendText("INFO: Navigating to home [" + My.Settings.home + "]" + vbNewLine)
+        DConsole.WriteLine("INFO: Navigating to home [" + My.Settings.home + "]" + vbNewLine)
     End Sub
     'Search Button
     Private Sub Searchbtn_Click(sender As Object, e As EventArgs) Handles searchbtn.Click
         'WebBrowser1.GoSearch() was replaced with a separate and easier to configure alternative due to the same reason I changed the Homebtn
         WebBrowser1.Navigate(My.Settings.search)
-        DConsole.DCOutput.AppendText("INFO: Navigating to search [" + My.Settings.search + "]" + vbNewLine)
+        DConsole.WriteLine("INFO: Navigating to search [" + My.Settings.search + "]" + vbNewLine)
     End Sub
     'Stop Button
     Private Sub Stopbtn_Click(sender As Object, e As EventArgs) Handles stopbtn.Click
         WebBrowser1.Stop()
-        DConsole.DCOutput.AppendText("ERROR: Stopped (by a user)" + vbNewLine)
+        DConsole.WriteLine("ERROR: Stopped (by a user)" + vbNewLine)
     End Sub
     'Settings Button
     Private Sub Settingsbtn_Click(sender As Object, e As EventArgs) Handles settingsbtn.Click
         Settings.Show()
-        DConsole.DCOutput.AppendText("INFO: Settings opened!" + vbNewLine)
+        DConsole.WriteLine("INFO: Settings opened!" + vbNewLine)
     End Sub
     'Print Button
     Private Sub Printbtn_Click(sender As Object, e As EventArgs) Handles printbtn.Click
         WebBrowser1.ShowPrintPreviewDialog()
-        DConsole.DCOutput.AppendText("INFO: Print (Preview) Dialog opened!" + vbNewLine)
+        DConsole.WriteLine("INFO: Print (Preview) Dialog opened!" + vbNewLine)
     End Sub
     'Properties Button
     Private Sub Property_Click(sender As Object, e As EventArgs) Handles safety.Click
         WebBrowser1.ShowPropertiesDialog()
-        DConsole.DCOutput.AppendText("INFO: Page Properties Dialog opened!" + vbNewLine)
+        DConsole.WriteLine("INFO: Page Properties Dialog opened!" + vbNewLine)
     End Sub
     'Save As... Button
     Private Sub Saveasbtn_Click(sender As Object, e As EventArgs) Handles saveasbtn.Click
         WebBrowser1.ShowSaveAsDialog()
-        DConsole.DCOutput.AppendText("INFO: Save As Dialog Opened!" + vbNewLine)
+        DConsole.WriteLine("INFO: Save As Dialog Opened!" + vbNewLine)
     End Sub
     'When a page finishes loading...
     Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
@@ -98,13 +98,13 @@
         'so gotta write a line to make it autoresize(also had to put it inside a container)
         WebBrowser1.Size = WebBrowser1.Document.Body.ScrollRectangle.Size
         urltb.Text = WebBrowser1.Url.ToString 'yes it is needed a way to see the exact url where the user is... to prevent bad redirections and stuff
-        DConsole.DCOutput.AppendText("INFO: Navigated to: " + WebBrowser1.Url.ToString() + vbNewLine)
+        DConsole.WriteLine("INFO: Navigated to: " + WebBrowser1.Url.ToString() + vbNewLine)
     End Sub
     'When a page changes title
     Private Sub WebBrowser1_DocumentTitleChanged(sender As Object, e As EventArgs) Handles WebBrowser1.DocumentTitleChanged
 
         Text = "Browser - [" + WebBrowser1.DocumentTitle + "]" 'sounds fun and I could do it... why not then
-        DConsole.DCOutput.AppendText("INFO: New Page Title: " + WebBrowser1.DocumentTitle + vbNewLine)
+        DConsole.WriteLine("INFO: New Page Title: " + WebBrowser1.DocumentTitle + vbNewLine)
         'TODO: Give a fuckin name to the browser
     End Sub
 
@@ -113,7 +113,7 @@
         'Fancy code goes here...
         If e.KeyCode = Keys.Enter Then
             Call Gobtn_Click(sender, e)
-            DConsole.DCOutput.AppendText("DEBUG: Enter key pressed while focusing on urltb!" + vbNewLine)
+            DConsole.WriteLine("DEBUG: Enter key pressed while focusing on urltb!" + vbNewLine)
         End If
     End Sub
     'Keeps the Properties Button Label fresh with Encryption Level data
@@ -125,7 +125,7 @@
             Safety1 = Safety2
             safety.Text = WebBrowser1.EncryptionLevel.ToString()
             'TODO: Replace the safety related text with a lock picture... so it looks less smart...yeaahh I still need to think about it
-            DConsole.DCOutput.AppendText("WARNING: Encryption Level changed: " + WebBrowser1.EncryptionLevel.ToString() + vbNewLine)
+            DConsole.WriteLine("WARNING: Encryption Level changed: " + WebBrowser1.EncryptionLevel.ToString() + vbNewLine)
         End If
 
     End Sub
@@ -133,13 +133,13 @@
     Private Sub WebBrowser1_CanGoBackChanged(sender As Object, e As EventArgs) Handles WebBrowser1.CanGoBackChanged
 
         backbtn.Enabled = WebBrowser1.CanGoBack
-        DConsole.DCOutput.AppendText("DEBUG: Back Button Enabled" + vbNewLine)
+        DConsole.WriteLine("DEBUG: Back Button Enabled" + vbNewLine)
     End Sub
     'Enables the Forward Button when it is useful
     Private Sub WebBrowser1_CanGoForwardChanged(sender As Object, e As EventArgs) Handles WebBrowser1.CanGoForwardChanged
 
         forwardbtn.Enabled = WebBrowser1.CanGoForward
-        DConsole.DCOutput.AppendText("DEBUG: Forward Button Enabled" + vbNewLine)
+        DConsole.WriteLine("DEBUG: Forward Button Enabled" + vbNewLine)
     End Sub
     'Monitors the page loading and reports errors (of itself)
     Private Sub WebBrowser1_ProgressChanged(ByVal sender As Object, ByVal e As WebBrowserProgressChangedEventArgs) Handles WebBrowser1.ProgressChanged
@@ -149,10 +149,15 @@
             ProgressBar1.Maximum = e.MaximumProgress
             ProgressBar1.Value = e.CurrentProgress
 
+            If (e.CurrentProgress.ToString = "0" = False) And (e.MaximumProgress.ToString = "0" = False) Then
+                DConsole.WriteLine("Progress:" + e.CurrentProgress.ToString() + "/" + e.MaximumProgress.ToString + vbNewLine)
+            End If
+
             If ProgressBar1.Value = ProgressBar1.Maximum Then
 
 
                 ProgressBar1.Value = ProgressBar1.Maximum
+                Return
 
             End If
         Catch ex As Exception
